1f9aa563ffd4630ebe8eddff9620670e98fd2e2a
Anand Kale <166801233+Anand-kale@users.noreply.github.com>
2024-09-11 18:02:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import sys
+
+# Print 'Hello World'
 print('Hello World')
-print(sys.argv)
+
+# Check if the correct number of arguments is provided
+if len(sys.argv) != 2:
+    print("Usage: python main.py <env_variable_value>")
+    sys.exit(1)
+
+# Retrieve the environment variable value from the command-line arguments
+env_variable_value = sys.argv[1]
+
+# Print the value of the environment variable
+print(f'Environment Variable Value: {env_variable_value}')
